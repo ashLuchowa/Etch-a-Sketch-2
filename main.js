28f705654a685b80a5f42c82ab08660d906f4178
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-28 21:00:03 +0800

    Redo grid

--- main.js
+++ main.js
@@ -1,30 +1,12 @@
-const canva = document.querySelector('.main-canva');
-const rows = document.getElementsByClassName('inner-grid');
-const cells = document.getElementsByClassName('cell');
+//Selectors
 
-//Default Grid
-function defaultGrid() {
-    gridRow(16);
-    gridColumn(16);
+
+
+
+//Grid
+function gridSize(setSize) {
+    
 }
 
-defaultGrid(gridRow);
-
-//Create Grid Row
-function gridRow(rowNum) {
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement('div');
-        canva.appendChild(row).className = 'inner-grid';
-    };
-};
-
-//Create grid Column
-function gridColumn(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className = 'cell';
-        };
-    };
-};
-
+//Default Grid Size
+gridSize(16);

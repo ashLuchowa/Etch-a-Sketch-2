a7f175bd2cd11ea08990f3b7f8b987820f4263cd
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-28 20:16:54 +0800

    Primary Grid build

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 const canva = document.querySelector('.main-canva');
 const rows = document.getElementsByClassName('inner-grid');
+const cells = document.getElementsByClassName('cell');
 
 //Default Grid
-function defaultGrid(x, y) {
-    gridRow(x);
-    gridColumn(y);
+function defaultGrid() {
+    gridRow(16);
+    gridColumn(16);
 }
-defaultGrid(10, 10);
+
+defaultGrid(gridRow);
 
 //Create Grid Row
 function gridRow(rowNum) {
-    for(r = 0; r < rowNum; r++) {
+    for (r = 0; r < rowNum; r++) {
         let row = document.createElement('div');
         canva.appendChild(row).className = 'inner-grid';
-    }
-}
+    };
+};
 
 //Create grid Column
 function gridColumn(cellNum) {
-}
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = 'cell';
+        };
+    };
+};
+

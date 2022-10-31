2071331b8dbc59aa194a6a1511787675b070bc4f
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-31 19:43:17 +0800

    misc

--- main.js
+++ main.js
@@ -2,39 +2,27 @@
 const canva = document.querySelector('.main-canva');
 const blackBtn = document.querySelector('button');
 
-
 //-------------- THE GRID --------------//
-//grid column
-function gridRow(cells) {
+//generate grid
+function grid(cells) {
+    canva.style.setProperty('grid-template-columns', `repeat(${cells}, 1fr)`);
+    canva.style.setProperty('grid-template-rows', `repeat(${cells}, 1fr)`);
+    
     for (let r = 0; r < cells; r++) {
-        const square = document.createElement('div');
-        square.classList.add('square-pixel');
-        canva.appendChild(square);
-        canva.style.setProperty('grid-template-columns', `repeat(${cells}, 1fr)`);
-    }
-}
-
-//grid row
-function gridColumn(cells) {
-    for (let r = 0; r < cells; r++) {
-        for (let c = 1; c < cells; c++) {
+        for (let c = 0; c < cells; c++) {
             const square = document.createElement('div');
             square.classList.add('square-pixel');
             canva.appendChild(square);
         }
     }
 }
-
-//default grid size
-function grid(cells) {
-    gridRow(cells);
-    gridColumn(cells);
-}
-grid(16);
-
-
+//set default grid
+grid(5);
 
 //-------------- SET SIZE BUTTON --------------//
 blackBtn.addEventListener('click', () => {
-    
-});
+    //find a way to remove last grid before generating a new one
+    grid(15);
+})
+
+

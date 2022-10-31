a4720076e9e2ad566107a8041cfeb8bb3b96db55
Ash Luchowa <ashwinluchowa@gmail.com>
2022-11-01 00:32:38 +0800

    Build functional grid

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 //-------------- SELECTORS --------------//
 const canva = document.querySelector('.main-canva');
-const blackBtn = document.querySelector('button');
+const sizeButton = document.querySelector('#size-btn');
+
 
 //-------------- THE GRID --------------//
 //generate grid
 function grid(cells) {
     canva.style.setProperty('grid-template-columns', `repeat(${cells}, 1fr)`);
-    canva.style.setProperty('grid-template-rows', `repeat(${cells}, 1fr)`);
-    
+    let squares = canva.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+
+    //rows
     for (let r = 0; r < cells; r++) {
+        const square = document.createElement('div');
+        square.classList.add('square-pixel');
+        canva.appendChild(square);
+    }
+    //columns
+    for (let r = 0; r < cells - 1; r++) {
         for (let c = 0; c < cells; c++) {
             const square = document.createElement('div');
             square.classList.add('square-pixel');
@@ -16,13 +25,11 @@ function grid(cells) {
         }
     }
 }
-//set default grid
-grid(5);
+//default size
+grid(16);
+
 
 //-------------- SET SIZE BUTTON --------------//
-blackBtn.addEventListener('click', () => {
-    //find a way to remove last grid before generating a new one
-    grid(15);
+sizeButton.addEventListener('click', () => {
+    grid(32);
 })
-
-

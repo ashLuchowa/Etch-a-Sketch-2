24655082a7a10e69a0b54adff9bb42b41259e85a
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-31 15:12:54 +0800

    Complete grid

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //Selectors
 const canva = document.querySelector('.main-canva');
+
+//default grid
 let gridSize = 16;
+canva.style.setProperty('grid-template-columns', `repeat(${gridSize}, 1fr)`)
+canva.style.setProperty('grid-template-rows', `repeat(${gridSize}, 1fr)`)
 
-
-//Create grid
+//Create grid cells
 //grid row
 for (let r = 0; r < gridSize; r++) {
     const square = document.createElement('div');
@@ -11,7 +14,7 @@ for (let r = 0; r < gridSize; r++) {
     canva.appendChild(square);
 
     //grid column
-    for (let c = 0; c < gridSize; c++) {
+    for (let c = 0; c < gridSize -1; c++) {
         const square = document.createElement('div');
         square.classList.add('square-pixel');
         canva.appendChild(square);

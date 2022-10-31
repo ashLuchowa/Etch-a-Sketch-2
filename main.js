9ede754429d6a6feb8702adf775bb81173b68a33
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-31 16:27:51 +0800

    Create defaultGrid function

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 //Selectors
 const canva = document.querySelector('.main-canva');
+const blackBtn = document.querySelector('button');
+let cells = 10;
+
+
+//-------------- THE GRID --------------//
 
 //default grid
-let cells = 16;
-canva.style.setProperty('grid-template-columns', `repeat(${cells}, 1fr)`)
+function defaultGrid(cells) {
+    canva.style.setProperty('grid-template-columns', `repeat(${cells}, 1fr)`);
 
-//Create grid cells
-//grid row
-for (let r = 0; r < cells; r++) {
-    const square = document.createElement('div');
-    square.classList.add('square-pixel');
-    canva.appendChild(square);
-
-    //grid column
-    for (let c = 0; c < cells - 1; c++) {
+    //Create grid cells
+    //grid row
+    for (let r = 0; r < cells; r++) {
         const square = document.createElement('div');
         square.classList.add('square-pixel');
         canva.appendChild(square);
+    
+        //grid column
+        for (let c = 0; c < cells - 1; c++) {
+            const square = document.createElement('div');
+            square.classList.add('square-pixel');
+            canva.appendChild(square);
+        }
     }
 }
+
+defaultGrid(10);

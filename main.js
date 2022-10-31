f98138c7821210b577d4a06dba9abe14364f88f4
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-31 15:20:08 +0800

    Clean up + change namings

--- main.js
+++ main.js
@@ -2,21 +2,26 @@
 const canva = document.querySelector('.main-canva');
 
 //default grid
-let gridSize = 16;
-canva.style.setProperty('grid-template-columns', `repeat(${gridSize}, 1fr)`)
-canva.style.setProperty('grid-template-rows', `repeat(${gridSize}, 1fr)`)
+let cells = 16;
+canva.style.setProperty('grid-template-columns', `repeat(${cells}, 1fr)`)
+
+//change grid size
+
 
 //Create grid cells
 //grid row
-for (let r = 0; r < gridSize; r++) {
+for (let r = 0; r < cells; r++) {
     const square = document.createElement('div');
     square.classList.add('square-pixel');
     canva.appendChild(square);
 
     //grid column
-    for (let c = 0; c < gridSize -1; c++) {
+    for (let c = 0; c < cells -1; c++) {
         const square = document.createElement('div');
         square.classList.add('square-pixel');
         canva.appendChild(square);
     }
 }
+
+//change grid size to 32
+gridSize(32);

d1cd86b724ffb81988ddbfa6f543c555f345a2ca
Ash Luchowa <ashwinluchowa@gmail.com>
2022-11-01 14:06:04 +0800

    Add color buttons + clear

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const canva = document.querySelector('.main-canva');
 const sizeBtn = document.querySelector('#size-btn');
 const blackBtn = document.querySelector('#black-btn');
 const redBtn = document.querySelector('#red-btn');
+const eraseBtn = document.querySelector('#erase-btn');
+const clearBtn = document.querySelector('#clear-btn');
 
 
 //-------------- THE GRID --------------//
@@ -12,13 +14,14 @@ function grid(cells) {
     squares.forEach((div) => div.remove());
     gridRow();
     gridColumn(cells);
-    gridColor();
+    clickColorGrid();
+
 }
 //default size
 grid(16);
 
 
-//-------------- FUNCTIONS --------------//
+//-------------- GRID FUNCTIONS --------------//
 //generate row
 function gridRow(cells) {
     for (let r = 0; r < cells; r++) {
@@ -37,20 +40,36 @@ function gridColumn(cells) {
         }
     }
 }
-//grid coloring
-function gridColor() {
+//mouse color
+function gridColor(color) {
     let pixels = document.querySelectorAll('.square-pixel');
+    color;
 
     for (let i = 0; i < pixels.length; i++) {
         pixels[i].addEventListener('mouseover', () => {
-            pixels[i].style.backgroundColor = 'black';
+            pixels[i].style.backgroundColor = `${color}`;
         });
     }
 }
+//click and color
+function clickColorGrid() {
+    blackBtn.addEventListener('click', () => {
+        gridColor('black');
+    });
+    redBtn.addEventListener('click', () => {
+        gridColor('red');
+    });
+    eraseBtn.addEventListener('click', () => {
+        gridColor('');
+    });
+    clearBtn.addEventListener('click', () => {
+        let squares = canva.querySelectorAll('div');
+        squares.forEach((div) => div.style.backgroundColor = '');
+    });
+}
 
 
-//-------------- BUTTONS --------------//
-//set size btn
+//-------------- SET SIZE --------------//
 sizeBtn.addEventListener('click', () => {
     let size = prompt('set a grid size', '1-100');
     size > 0 && size <= 100 ? grid(size) : grid(16);
